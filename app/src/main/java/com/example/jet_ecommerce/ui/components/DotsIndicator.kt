9ca93cb8435187593b13700d0d0093e51fb48a7e
MuhammedRefaatMetwally <114253447+MuhammedRefaatMetwally@users.noreply.github.com>
2023-11-13 15:16:18 +0200

    refactored dots indicator

--- app/src/main/java/com/example/jet_ecommerce/ui/components/DotsIndicator.kt
+++ app/src/main/java/com/example/jet_ecommerce/ui/components/DotsIndicator.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -11,14 +12,21 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jet_ecommerce.R
 
 @Composable
 fun DotsIndicator(
+    modifier: Modifier = Modifier,
+    secondStyle : Boolean = false,
     totalDots : Int,
     selectedIndex : Int,
     selectedColor: Color,
@@ -27,7 +35,7 @@ fun DotsIndicator(
 ){
 
     LazyRow(
-        modifier = Modifier
+        modifier = modifier
             .wrapContentWidth()
             .wrapContentHeight()
             .padding(bottom = 24.dp)
@@ -36,29 +44,43 @@ fun DotsIndicator(
 
         items(totalDots) { index ->
             if (index == selectedIndex) {
-                Box(
-                    modifier = Modifier
-                        .size(10.dp)
-                        .clip(CircleShape)
-                        .background(selectedColor)
+                if(secondStyle){
+                    Box(
+                        modifier = Modifier
+                            .width(30.dp)
+                            .height(7.dp)
+                            .clip(RoundedCornerShape(CornerSize(30.dp)))
+                            .background(colorResource(id = R.color.main_color))
 
-                )
+                    )
+                   
+                }else{
+                    Box(
+                        modifier = modifier
+                            .size(10.dp)
+                            .clip(CircleShape)
+                            .background(selectedColor)
+
+                    )
+                }
+
 
             } else {
                 Box(
-                    modifier = Modifier
+                    modifier = modifier
                         .size(10.dp)
                         .clip(CircleShape)
                         .background(unSelectedColor)
-                        .clickable{
+                        .clickable {
                             onDotClick.invoke(index)
                         }
                 )
             }
 
             if (index != totalDots - 1) {
-                Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+                Spacer(modifier = modifier.padding(horizontal = 2.dp))
             }
         }
     }
 }
+
